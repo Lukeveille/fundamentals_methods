769b163c7b5c11ce166ec82a6338e120c7e2fa37
Lukeveille <luke@lukeleveille.com>
2018-09-05 16:08:58 -0400

    Completed exercise 3

---
+++ exercise3.rb
@@ -0,0 +1,13 @@
+def is_even?(num)
+    if num % 2 == 0
+        return true
+    else
+        return false
+    end
+end
+
+puts is_even?(10)
+puts is_even?(13)
+puts is_even?(30)
+puts is_even?(35)
+puts is_even?(100)
